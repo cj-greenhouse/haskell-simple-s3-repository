eeda3b30cd7bb89422a95df82d3f0caca607582f
Greg Wiley <gwiley@cj.com>
2019-03-18 09:55:25 -0700

    write index

--- test/Spec/GenerateIndex.hs
+++ test/Spec/GenerateIndex.hs
@@ -5,48 +5,66 @@ import Spec.Prelude
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Writer
-import Data.Set (Set, fromList, singleton)
+import Data.List (find)
+import Data.Set (Set, fromList)
 import Data.Text (Text)
 
 
 tests :: TestTree
 tests = testGroup "GenerateIndex" [
 
-    testCase "fetches every package" $ do
+    testGroup ("flow") [
 
-        let
-        -- given
-            packages = ["foo", "bar"]
+        testCase "writes index" $ do
+            let
+            -- given
+                packages = [
+                    Pkg "config1" (PkgInfo "foo" "1.1.1"),
+                    Pkg "config2" (PkgInfo "bar" "0.1.1.3")]
 
-        -- when
-            actual = generate
+            -- when
+                actual = generate
 
-        -- then
-        captured packages actual === fromList packages
+            -- then
+            captured packages actual === [fromList packages]
 
-
+        ]
     ]
 
 ---------------------------------------------------------------
 -- Actual
 
+type Cabal = Text
+data Package = Pkg {_cabal :: Cabal, _info :: PackageInfo} deriving (Eq, Show, Ord)
+data PackageInfo = PkgInfo {_name :: Text, _version :: Text} deriving (Eq, Show, Ord)
+type Index = Set Package
+
+
 generate :: (Monad m, PackageStore m) => m ()
-generate = listPackages >>= mapM_ fetchPackage
+generate = do
+    infos <- listPackages
+    cabals <- mapM fetchPackage infos
+    let packages = zipWith Pkg cabals infos
+    storeIndex $ fromList packages
 
 class PackageStore m where
-    listPackages :: m [Text]
-    fetchPackage :: Text -> m ()
+    listPackages :: m [PackageInfo]
+    fetchPackage :: PackageInfo -> m Cabal
+    storeIndex :: Index -> m ()
 
 ----------------------------------------------------------------
 -- Test
 
-type Capture = Set Text
-type Env = [Text]
+type Capture = [Index]
+type Env = [Package]
 type Test = ReaderT Env (Writer Capture)
 
 captured :: Env -> Test a -> Capture
 captured env = snd . runWriter . flip runReaderT env
 
 instance PackageStore Test where
-    listPackages =  ask
-    fetchPackage = lift . tell . singleton
+    listPackages =  ask >>= pure . fmap _info
+    fetchPackage info = do
+        ask >>= pure . _cabal . maybe undefined id . find (\a -> (_info a == info))
+    storeIndex  = lift . tell . (: [])
+
