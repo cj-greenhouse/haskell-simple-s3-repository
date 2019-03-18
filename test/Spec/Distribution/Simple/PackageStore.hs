68128ca44494524f2b440768a6c3029e51b45891
Greg Wiley <gwiley@cj.com>
2019-03-18 13:31:51 -0700

    invariant in test

--- test/Spec/Distribution/Simple/PackageStore.hs
+++ test/Spec/Distribution/Simple/PackageStore.hs
@@ -5,35 +5,62 @@ module Spec.Distribution.Simple.PackageStore (tests) where
 import Spec.Prelude
 
 import Control.Monad.Trans.Reader
-import Data.Text (Text, unpack)
-import Distribution.Text (simpleParse)
+import Data.Text (Text, unpack, pack)
+import Distribution.Pretty (prettyShow)
+import Distribution.Types.PackageId (PackageIdentifier (..), PackageId)
+import Distribution.Types.PackageName (mkPackageName)
+import Distribution.Types.Version (mkVersion)
 
 import Distribution.Simple.PackageStore
 
 tests :: TestTree
 tests = testGroup "PackageStore" [
 
-    testCase "filter and parse object names" $ do
-        let
-            n1 = "mylib"; v1 = "0.1.3.2"
-            n2 = "other"; v2 = "3.2.1"
-            n3 = "why"; v3 = "why"
-            listing = [
-                "wot", n1 <> "-" <> v1 <> ".tar.gz",
-                "99.33.2", n2 <> "-" <> v2 <> ".tar.gz",
-                "other",
-                "index.tar.gz",
-                n3 <> "-" <> v3 <> ".tar.gz",
-                "more"]
+    testGroup "object store impl" [
+        testCase "filter and parse object names" $ do
+            let
+            -- given
+                n1 = "mylib"; v1 = [0,1,3,2]
+                n2 = "other"; v2 = [3,2,1]
+                n3 = "why"; v3 = [2]
+                e1 = pid n1 v1
+                e2 = pid n2 v2
+                e3 = pid n3 v3
+                listing = [
+                    pidt e1 <> ".tar.gz",
+                    "99.33.2",
+                    pidt e2 <> ".tar.gz",
+                    "other",
+                    "index.tar.gz",
+                    pidt e3 <> ".tar.gz",
+                    "more"]
 
-            actual = listPackagesUsingObjectStore
+            -- when
+                actual = listPackagesUsingObjectStore
 
-            e1 = maybe undefined id $ simpleParse . unpack $ n1 <> "-" <> v1
-            e2 = maybe undefined id $ simpleParse . unpack $ n2 <> "-" <> v2
-            e3 = maybe undefined id $ simpleParse . unpack $ n3 <> "-" <> v3
-        value listing actual === [e1, e2, e3]
+            -- then
+            value listing actual === [e1, e2, e3]
+        -- ,
+        -- testCase "fetch package" $ do
+        --     let
+        --     -- given
+        --         n = "thelib"; v = "0.1.1.1.1.1"
+        --         pkg =
+        --     -- when
+
+        --     -- then
+
+        ]
+
 
     ]
+
+
+pid :: Text -> [Int] -> PackageId
+pid n v = PackageIdentifier (mkPackageName (unpack n)) (mkVersion v)
+
+pidt :: PackageId -> Text
+pidt = pack . prettyShow
 
 type Env = [Text]
 type Test = Reader Env
